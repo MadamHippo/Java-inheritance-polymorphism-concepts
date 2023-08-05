7ff0ecf639ad1810da3cc5ca2290a4bd3a40ab96
LilyTheDev <88348716+MadamHippo@users.noreply.github.com>
2023-08-05 00:09:19 -0700

    Update Spaghetti.java

--- Spaghetti.java
+++ Spaghetti.java
@@ -1,16 +1,17 @@
+// This class represents a specific type of Noodle called Spaghetti
 class Spaghetti extends Noodle {
   
+  // Constructor for Spaghetti class
   Spaghetti() {
-    
+    // Calling the superclass constructor (Noodle) with specific values
   	super(30.0, 0.2, "round", "semolina flour");
-    
   }
   
+  // Overriding the getCookPrep() method from the superclass (Noodle)
   @Override
   public String getCookPrep() {
-    
+    // Returning the cooking instructions for spaghetti
     return "Boil spaghetti for 8 - 12 minutes and add sauce, cheese, or oil and garlic.";
-    
   }
   
 }
